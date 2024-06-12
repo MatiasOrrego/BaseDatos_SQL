8cc1d9a14dfa15a8eaeedc02b50de528aa65994d
OrregoMatias <orregomatias50@gmail.com>
2024-06-12 12:32:25 -0300

    agregue mas consultas

--- consultas.sql
+++ consultas.sql
@@ -44,3 +44,55 @@ select * from productos where descuento is not null;
 select * from usuarios where email is null;
 /*Consulta filtrando por valores nulos, en este caso se busca los usuarios que no tengan email, para eso se utiliza IS NULL*/
 
+////////////////////////////////////////////
+ORDER BY
+////////////////////////////////////////////
+
+select * from usuarios order by nombre;
+/*Consulta ordenando los resultados, en este caso se ordena por nombre*/
+select precio from productos order by precio desc; 
+/*Consulta ordenando los resultados, en este caso se ordena por precio de forma desc (descendente) y asc(ascendente)*/
+
+select * from productos order by precio Nulls last;
+/*Consulta ordenando los resultados, en este caso se ordena por precio y los valores nulos se colocan al final*/
+
+select * from empleados order by salario asc, nombre asc;
+/*Consulta ordenando los resultados, en este caso se ordena por salario de forma ascendente y por nombre de forma ascendente*/
+
+select * from productos order by stock desc, color asc ;
+/*Consulta ordenando los resultados, en este caso se ordena por stock de forma descendente y por color de forma ascendente*/
+
+////////////////////////////////////////////
+LIMIT
+////////////////////////////////////////////
+
+select * from usuarios order by id limit 3;
+/*Consulta limitando los resultados, en este caso se limita a 3 resultados*/
+
+select puntaje from puntajes order by puntaje desc limit 1;
+/*Consulta limitando los resultados, en este caso se limita a 1 resultado (regresa el resultado mas alto de la tabla)*/
+
+select nombre_concierto, entradas_vendidas from conciertos 
+order by entradas_vendidas desc 
+limit 1;
+/*Consulta limitando los resultados, en este caso se limita a 1 resultado (regresa el concierto con mas entradas vendidas)*/
+
+////////////////////////////////////////////
+Operacion con Strings
+////////////////////////////////////////////
+
+select upper (email) as email_upper 
+from usuarios;
+/*Consulta que convierte a mayusculas el contenido de una columna y le da un alias*/
+
+select lower (email) as email_lower from usuarios;
+/*Consulta que convierte a minusculas el contenido de una columna y le da un alias*/
+
+select trim(nombre), trim(email) from usuarios;
+/*Consulta que elimina los espacios en blanco al inicio y al final de una cadena*/
+
+select trim (nombre) as nombre_limpio, 
+trim (apellido) as apellido_limpio,
+trim (email) as email_limpio
+from usuarios;
+/*Consulta que elimina los espacios en blanco al inicio y al final de una cadena y le da un alias*/
